9c544a5ac7b6cd88e5295df5cfa95c37bd4b155e
lnschroeder <33390735+lnschroeder@users.noreply.github.com>
2023-06-27 09:41:49 +0200

    Add Graph data structure

---
+++ Main.hs
@@ -0,0 +1,31 @@
+{-# Language DuplicateRecordFields #-}
+
+data NodeType = Account | Device | Biometric | Knowledge deriving (Show)
+data Node = Node {
+    id :: Integer,
+    name :: String,
+    ofType :: NodeType,
+    protectedBy :: [Access]
+} deriving (Show)
+
+
+data AccessType = Primary | Recovery deriving (Show)
+data Access = Access {
+    nodes :: [Node],
+    ofType :: AccessType
+} deriving (Show)
+
+newtype Graph = Graph {
+    nodes :: [Node]
+} deriving (Show)
+
+addNode :: Graph -> Node -> Graph
+-- TODO Urs 
+
+addAccess :: Graph -> Node -> Access -> Graph
+-- TODO Niklas
+
+-- main function
+main :: IO ()
+main = do 
+    putStrLn "Hello World"
