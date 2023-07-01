f233373264efeeca7bbcf7eead987dca7b866f11
Urs Kahmann <urka3288@bht-berlin.de>
2023-07-01 21:07:05 +0200

    Simplify addProtectedBy function

--- Main.hs
+++ Main.hs
@@ -1,7 +1,9 @@
+import Data.List (find)
+
 data Node = Node {
     name :: String,
     protectedBy :: [[Node]]
-} deriving (Show)
+} deriving (Show, Eq)
 
 newtype Graph = Graph {
     nodes :: [Node]
@@ -10,15 +12,23 @@ newtype Graph = Graph {
 addNode :: Graph -> Node -> Graph
 addNode (Graph g) n = Graph (g ++ [n])
 
+getNode :: String -> Graph -> Maybe Node
+getNode s (Graph g) = find (\x -> name x == s) g
+
+getNodes :: Graph -> [String] -> [Node]
+getNodes g s = map (\(Just x) -> x)  $ filter (/= Nothing)  $ map (`getNode` g) s
+    
 -- adds nodes to the protectedBy field for a given node
-addProtectedBy :: Graph -> String -> [Node] -> Graph
+addProtectedBy :: Graph -> String -> [String] -> Graph
 addProtectedBy g = addProtectedBy' g (Graph [])
 
-addProtectedBy' :: Graph -> Graph -> String -> [Node] -> Graph 
+addProtectedBy' :: Graph -> Graph -> String -> [String] -> Graph 
 addProtectedBy' (Graph []) g _ _ = g
-addProtectedBy' (Graph (x:xs)) (Graph new) nname accesses
-    | name x == nname = addProtectedBy' (Graph xs) (Graph (new ++ [Node nname (protectedBy x ++ [accesses])])) nname accesses
-    | otherwise = addProtectedBy' (Graph xs) (Graph (new ++ [x])) nname accesses
+addProtectedBy' (Graph (x:xs)) (Graph new) nname nnames
+    | name x == nname = addProtectedBy' (Graph xs) (Graph (new ++ [Node nname (protectedBy x ++ [accesses])])) nname nnames
+    | otherwise = addProtectedBy' (Graph xs) (Graph (new ++ [x])) nname nnames
+    where
+        accesses = getNodes (Graph $ (x:xs) ++ new) nnames
 
 printMermaidProtectedBy :: [Node] -> String -> String
 printMermaidProtectedBy [] n = ""
@@ -46,16 +56,16 @@ main = do
         updatedGraph2 = addNode updatedGraph1 Node { name = "Phone", protectedBy = [] }
         updatedGraph3 = addNode updatedGraph2 Node { name = "Finger", protectedBy = [] }
         updatedGraph4 = addNode updatedGraph3 Node { name = "Bitwarden", protectedBy = [] }
-        updatedGraph5 = addProtectedBy updatedGraph4 "Bitwarden" [Node { name = "pw_Bitwarden", protectedBy = [] }]
-        updatedGraph6 = addProtectedBy updatedGraph5 "Bitwarden" [Node { name = "Finger", protectedBy = [] }, Node { name = "Phone", protectedBy = [] }]
+        updatedGraph5 = addProtectedBy updatedGraph4 "Bitwarden" ["pw_Bitwarden"]
+        updatedGraph6 = addProtectedBy updatedGraph5 "Bitwarden" ["Finger", "Phone"]
         -- putStrLn "Graph1:"
-    putStrLn "Test"
-    print initialGraph
-    print updatedGraph1
-    print updatedGraph2
-    print updatedGraph3
-    print updatedGraph4
-    print updatedGraph5
+    -- putStrLn "Test"
+    -- print initialGraph
+    -- print updatedGraph1
+    -- print updatedGraph2
+    -- print updatedGraph3
+    -- print updatedGraph4
+    -- print updatedGraph5
     putStrLn (printMermaidContent updatedGraph6)
     
     -- updatedGraph2 = test updatedGraph Node { name = "Finger" }
