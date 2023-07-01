e4a4ae66d8d0642a19f14aa6e84f7b9af6a2fb61
Urs Kahmann <urka3288@bht-berlin.de>
2023-07-01 19:03:51 +0200

    WIP implement addProtectedBy function

--- Main.hs
+++ Main.hs
@@ -1,17 +1,6 @@
-{-# Language DuplicateRecordFields #-}
-
-data NodeType = Account | Device | Biometric | Knowledge deriving (Show)
 data Node = Node {
     name :: String,
-    ofType :: NodeType,
-    protectedBy :: [Access]
-} deriving (Show)
-
-
-data AccessType = Primary | Recovery deriving (Show)
-data Access = Access {
-    nodes :: [Node],
-    ofType :: AccessType
+    protectedBy :: [[Node]]
 } deriving (Show)
 
 newtype Graph = Graph {
@@ -19,12 +8,36 @@ newtype Graph = Graph {
 } deriving (Show)
 
 addNode :: Graph -> Node -> Graph
-addNode (Graph g) n = (Graph (g ++ [n]))
+addNode (Graph g) n = Graph (g ++ [n])
 
--- addAccess :: Graph -> Node -> Access -> Graph
--- addAccess graph node access = 
+-- adds nodes to the protectedBy field for a given node
+addProtectedBy :: Graph -> String -> [Node] -> Graph
+addProtectedBy g = addProtectedBy' g (Graph [])
 
--- main function
+addProtectedBy' :: Graph -> Graph -> String -> [Node] -> Graph 
+addProtectedBy' (Graph []) g _ _ = g
+addProtectedBy' (Graph (x:xs)) (Graph new) nname accesses
+    | name x == nname = addProtectedBy' (Graph xs) (Graph (new ++ [Node nname (protectedBy x ++ [accesses])])) nname accesses
+    | otherwise = addProtectedBy' (Graph xs) (Graph (new ++ [x])) nname accesses
+
 main :: IO ()
-main = do 
-    putStrLn "Hello World"
+main = do
+    let 
+        initialGraph = Graph []
+        updatedGraph1 = addNode initialGraph Node { name = "pw_Bitwarden", protectedBy = [[]] }
+        updatedGraph2 = addNode updatedGraph1 Node { name = "Phone", protectedBy = [[]] }
+        updatedGraph3 = addNode updatedGraph2 Node { name = "Finger", protectedBy = [[]] }
+        updatedGraph4 = addNode updatedGraph3 Node { name = "Bitwarden", protectedBy = [[]] }
+        updatedGraph5 = addProtectedBy updatedGraph4 "Bitwarden" [Node { name = "pw_Bitwarden", protectedBy = [[]] }]
+        -- putStrLn "Graph1:"
+    putStrLn "Test"
+    print initialGraph
+    print updatedGraph1
+    print updatedGraph2
+    print updatedGraph3
+    print updatedGraph4
+    print updatedGraph5
+
+    -- updatedGraph2 = test updatedGraph Node { name = "Finger" }
+
+-- Graph {nodes = [Node {name = "pw_Bitwarden", protectedBy = [[]]},Node {name = "Phone", protectedBy = [[]]},Node {name = "Finger", protectedBy = [[]]}]}
