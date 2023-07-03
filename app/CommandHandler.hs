62d283e716c6f03ba2fdecf1c96a5b7337b66313
lnschroeder <33390735+lnschroeder@users.noreply.github.com>
2023-07-03 17:33:37 +0200

    Add "add access" via command line

--- app/CommandHandler.hs
+++ app/CommandHandler.hs
@@ -3,24 +3,29 @@ module CommandHandler (
 ) where
     
 import Data.List (isPrefixOf)
+import Data.List.Split (splitOn)
 import Graphviz (printGraph)
 import qualified AccountAccessGraph as AAG
+import System.IO
 
-extractCommandParameter :: String -> String -> String
-extractCommandParameter cmd = drop (length cmd)
-
-    -- writeFile "graph.dot" (printGraph updatedGraph6)
+extractCommandParameters :: String -> Int -> [String]
+extractCommandParameters cmd l = drop l (splitOn " " cmd)
 
 invoke :: String -> AAG.Graph -> (String, AAG.Graph)
 invoke cmd graph
     | "add node " `isPrefixOf` cmd = do
-        let name = extractCommandParameter "add node " cmd
+        let name = head $ extractCommandParameters cmd 2
         ("Added node " ++ name, AAG.addNode name graph)
+    | "add access " `isPrefixOf` cmd = do
+        let nodes = extractCommandParameters cmd 2 
+        ("Added access " ++ show nodes, AAG.addProtectedBy (head nodes) (tail nodes) graph)
     | otherwise = ("Unknown command", graph)
 
 
 commandHandler :: AAG.Graph -> IO ()
 commandHandler g = do
+    putStr "> "
+    hFlush stdout
     name <- getLine
     let (message, graph) = invoke name g
     writeFile "graph.dot" (printGraph graph)
--- app/Main.hs
+++ app/Main.hs
@@ -5,11 +5,5 @@ main :: IO ()
 main = do
     let 
         initialGraph = []
-        -- updatedGraph1 = addNode Node { name = "pw_Bitwarden", protectedBy = [] } initialGraph 
-        -- updatedGraph2 = addNode Node { name = "Phone", protectedBy = [] } updatedGraph1 
-        -- updatedGraph3 = addNode Node { name = "Finger", protectedBy = [] } updatedGraph2 
-        -- updatedGraph4 = addNode Node { name = "Bitwarden", protectedBy = [] } updatedGraph3 
-        -- updatedGraph5 = addProtectedBy "Bitwarden" ["pw_Bitwarden"] updatedGraph4
-        -- updatedGraph6 = addProtectedBy "Bitwarden" ["Finger", "Phone"] updatedGraph5
-    putStrLn "Have fun!"
+    putStrLn "Welcome to your account access graph. Use help for a list of commands."
     CommandHandler.commandHandler initialGraph
