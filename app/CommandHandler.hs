8b978ea81d3268010d4b6abbfbc51f841e7b49c3
lnschroeder <33390735+lnschroeder@users.noreply.github.com>
2023-07-04 09:07:08 +0200

    Add helper command
    Co-authored-by: UrsKahmann <urka3288@bht-berlin.de>

--- app/CommandHandler.hs
+++ app/CommandHandler.hs
@@ -19,6 +19,8 @@ invoke cmd graph
     | "add access " `isPrefixOf` cmd = do
         let nodes = extractCommandParameters cmd 2 
         ("Added access " ++ show nodes, AAG.addProtectedBy (head nodes) (tail nodes) graph)
+    | "help" `isPrefixOf` cmd = do
+        ("available commands:\n   add node <node name>\n\t - adds a new node to the graph\n   add access <node name> <protector1> <protector2> ...\n\t - adds a list of nodes (protectors) to a given node as access", graph)
     | otherwise = ("Unknown command", graph)
 
 
